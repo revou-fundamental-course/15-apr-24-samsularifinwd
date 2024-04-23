c7e5a926b7fa888e58a5396fbb4b1f11a9cf3cba
Samsul <samsularifinwd@gmail.com>
2024-04-23 15:06:22 +0700

    perbarui

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-let celciusInput = document.querySelector('#celcius > input')
-let fahrenheitInput = document.querySelector('#fahrenheit > input')
-let kelvinInput = document.querySelector('#kelvin > input')
+// Menyimpan Input
+let celciusInput = document.querySelector('#celcius > input');
+let fahrenheitInput = document.querySelector('#fahrenheit > input');
 
+// Tombol Reset
 let btn = document.querySelector('.button button');
 
+// Fungsi untuk membulatkan angka
 function roundNumber(number){
   return Math.round(number*100)/100
 }
 
+//
 celciusInput.addEventListener('input', function(){
   let cTemp = parseFloat(celciusInput.value)
   let fTemp = (cTemp*(9/5)) + 32
-  let kTemp = cTemp + 273.15
   
-  fahrenheitInput.value = roundNumber(fTemp)
-  kelvinInput.value = roundNumber(kTemp)
-})
+  fahrenheitInput.value = roundNumber(fTemp);
+});
+
+//
+fahrenheitInput.addEventListener('input', function(){
+  let fTemp = parseFloat(fahrenheitInput.value);
+  let cTemp = (fTemp - 32) * (5/9);
+  
+  celciusInput.value = roundNumber(cTemp);
+});
+
+// Untuk mereset semua input
+btn.addEventListener('click', function(){
+  celciusInput.value = '';
+  fahrenheitInput.value = '';
+});
