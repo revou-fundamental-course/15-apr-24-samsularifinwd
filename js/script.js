f61e43738bfa7c16a319c0837f92093408ec034e
Samsul <samsularifinwd@gmail.com>
2024-04-23 13:49:17 +0700

    memperbaiki file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-let celciusInput = document.querySelector('#celcius > input');
-let fahrenheitInput = document.querySelector('#fahrenheit > input');
-let kelvinInput = document.querySelector('#kelvin > input');
+let celciusInput = document.querySelector('#celcius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
 
 let btn = document.querySelector('.button button');
 
 function roundNumber(number){
   return Math.round(number*100)/100
 }
+
+celciusInput.addEventListener('input', function(){
+  let cTemp = parseFloat(celciusInput.value)
+  let fTemp = (cTemp*(9/5)) + 32
+  let kTemp = cTemp + 273.15
+  
+  fahrenheitInput.value = roundNumber(fTemp)
+  kelvinInput.value = roundNumber(kTemp)
+})
